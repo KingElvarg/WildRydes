684ca1a8cd32da694c87a672c55723fe65a8da69
student <kkoryck1@jh.edu>
2026-02-16 15:48:51 -0500

    config update

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_I8PWaxQ3R', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6rsh84dd06botnf132tkrne13r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1_I8PWaxQ3R' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
